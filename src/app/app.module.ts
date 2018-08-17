c080eaac4b6cd2143069b42f35984628aad3e2ec
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-17 12:14:30 +0300

    added a route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { UserComponent } from './user/user.component';
 
+const routes:Routes=[
+  {path:"landing",component:LandingComponent},
+  {path:"user",component:UserComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    UserComponent,
+    
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
