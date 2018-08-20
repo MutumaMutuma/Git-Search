8c34335cdca551675b280ab0de68d1a438ea1d96
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-20 10:04:02 +0300

    njdnv

--- src/app/gitsearch-http/gitsearch-request.service.ts
+++ src/app/gitsearch-http/gitsearch-request.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import { Git } from '../git';
 @Injectable({
   providedIn: 'root'
 })
 export class GitsearchRequestService {
-
+  git: Git;
   constructor() { }
 }
