b1f3bfa1906bd82ea75051be1fa3c3524f640d8a
MutumaMutuma <lewismutuma1000@gmail.com>
2018-08-20 10:02:09 +0300

    added an inject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {UserService} from './users/user.service';
+import {GitsearchRequestService} from './gitsearch-http/gitsearch-request.service';
 // import { NgProgressModule } from 'ngx-progressbar';
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
@@ -25,7 +25,7 @@ const routes:Routes=[
     HttpClientModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [UserService],
+  providers: [GitsearchRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
